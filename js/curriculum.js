312fa6ea5868f623ba39a91e6063d1659cef93bf
ready4exam <ready4urexam@gmail.com>
2025-12-22 21:14:48 +0530

    chore: update table_id for Floating Bodies

--- js/curriculum.js
+++ js/curriculum.js
@@ -1,88 +1,333 @@
 export const curriculum = {
   "Science": {
     "Physical Science (Physics & Chemistry)": [
-      { "chapter_title": "Matter Around Us", "table_id": "", "section": "Science" },
-      { "chapter_title": "Motion", "table_id": "", "section": "Science" },
-      { "chapter_title": "Laws of Motion", "table_id": "", "section": "Science" },
-      { "chapter_title": "Refraction of Light at Plane Surfaces", "table_id": "", "section": "Science" },
-      { "chapter_title": "Gravitation", "table_id": "", "section": "Science" },
-      { "chapter_title": "Is Matter Pure?", "table_id": "", "section": "Science" },
-      { "chapter_title": "Atoms, Molecules and Chemical Reactions", "table_id": "", "section": "Science" },
-      { "chapter_title": "Floating Bodies", "table_id": "", "section": "Science" },
-      { "chapter_title": "What is Inside the Atom?", "table_id": "", "section": "Science" },
-      { "chapter_title": "Work and Energy", "table_id": "", "section": "Science" },
-      { "chapter_title": "Heat", "table_id": "", "section": "Science" },
-      { "chapter_title": "Sound", "table_id": "", "section": "Science" }
+      {
+        "chapter_title": "Matter Around Us",
+        "table_id": "",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Motion",
+        "table_id": "",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Laws of Motion",
+        "table_id": "",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Refraction of Light at Plane Surfaces",
+        "table_id": "",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Gravitation",
+        "table_id": "",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Is Matter Pure?",
+        "table_id": "",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Atoms, Molecules and Chemical Reactions",
+        "table_id": "",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Floating Bodies",
+        "table_id": "science_floating_bodies_9tg_quiz",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "What is Inside the Atom?",
+        "table_id": "",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Work and Energy",
+        "table_id": "",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Heat",
+        "table_id": "",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Sound",
+        "table_id": "",
+        "section": "Science"
+      }
     ],
     "Biological Science": [
-      { "chapter_title": "Cell - Its Structure and Functions", "table_id": "", "section": "Science" },
-      { "chapter_title": "Plant Tissues", "table_id": "", "section": "Science" },
-      { "chapter_title": "Animal Tissues", "table_id": "", "section": "Science" },
-      { "chapter_title": "Plasma Membrane", "table_id": "", "section": "Science" },
-      { "chapter_title": "Diversity in Living Organisms", "table_id": "", "section": "Science" },
-      { "chapter_title": "Sense Organs", "table_id": "", "section": "Science" },
-      { "chapter_title": "Animal Behaviour", "table_id": "", "section": "Science" },
-      { "chapter_title": "Challenges in Improving Agricultural Products", "table_id": "", "section": "Science" },
-      { "chapter_title": "Adaptations in Different Ecosystems", "table_id": "", "section": "Science" },
-      { "chapter_title": "Soil Pollution", "table_id": "", "section": "Science" },
-      { "chapter_title": "Biogeochemical Cycles", "table_id": "", "section": "Science" }
+      {
+        "chapter_title": "Cell - Its Structure and Functions",
+        "table_id": "",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Plant Tissues",
+        "table_id": "",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Animal Tissues",
+        "table_id": "",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Plasma Membrane",
+        "table_id": "",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Diversity in Living Organisms",
+        "table_id": "",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Sense Organs",
+        "table_id": "",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Animal Behaviour",
+        "table_id": "",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Challenges in Improving Agricultural Products",
+        "table_id": "",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Adaptations in Different Ecosystems",
+        "table_id": "",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Soil Pollution",
+        "table_id": "",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Biogeochemical Cycles",
+        "table_id": "",
+        "section": "Science"
+      }
     ]
   },
-
   "Social Studies": {
     "Geography & Economics": [
-      { "chapter_title": "Our Earth", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "The Natural Realms of the Earth", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "Hydrosphere", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "Atmosphere", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "Biosphere", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "Agriculture in India", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "Industries in India", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "Service Activities in India", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "Prices and Cost of Living", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "Government Budget and Taxation", "table_id": "", "section": "Social Studies" }
+      {
+        "chapter_title": "Our Earth",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "The Natural Realms of the Earth",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "Hydrosphere",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "Atmosphere",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "Biosphere",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "Agriculture in India",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "Industries in India",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "Service Activities in India",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "Prices and Cost of Living",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "Government Budget and Taxation",
+        "table_id": "",
+        "section": "Social Studies"
+      }
     ],
     "History & Political Science": [
-      { "chapter_title": "The Industrial Revolution", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "Women Protection Acts", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "Democratic and Nationalist Revolutions: 17th, 18th & 19th Centuries", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "Democratic Government", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "Changing Cultural Traditions in Europe (1300-1800)", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "Social Protest Movements", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "Colonialism in Latin America, Asia and Africa", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "Impact of Colonialism in India", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "Expansion of Democracy", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "Democracy - An Evolving Idea", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "Human Rights and Fundamental Rights", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "Disaster Management", "table_id": "", "section": "Social Studies" },
-      { "chapter_title": "Traffic Education", "table_id": "", "section": "Social Studies" }
+      {
+        "chapter_title": "The Industrial Revolution",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "Women Protection Acts",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "Democratic and Nationalist Revolutions: 17th, 18th & 19th Centuries",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "Democratic Government",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "Changing Cultural Traditions in Europe (1300-1800)",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "Social Protest Movements",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "Colonialism in Latin America, Asia and Africa",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "Impact of Colonialism in India",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "Expansion of Democracy",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "Democracy - An Evolving Idea",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "Human Rights and Fundamental Rights",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "Disaster Management",
+        "table_id": "",
+        "section": "Social Studies"
+      },
+      {
+        "chapter_title": "Traffic Education",
+        "table_id": "",
+        "section": "Social Studies"
+      }
     ]
   },
-
   "Mathematics": {
     "Number Systems & Algebra": [
-      { "chapter_title": "Real Numbers", "table_id": "", "section": "Mathematics" },
-      { "chapter_title": "Polynomials and Factorisation", "table_id": "", "section": "Mathematics" },
-      { "chapter_title": "Linear Equations in Two Variables", "table_id": "", "section": "Mathematics" },
-      { "chapter_title": "Algebraic Identities", "table_id": "", "section": "Mathematics" }
+      {
+        "chapter_title": "Real Numbers",
+        "table_id": "",
+        "section": "Mathematics"
+      },
+      {
+        "chapter_title": "Polynomials and Factorisation",
+        "table_id": "",
+        "section": "Mathematics"
+      },
+      {
+        "chapter_title": "Linear Equations in Two Variables",
+        "table_id": "",
+        "section": "Mathematics"
+      },
+      {
+        "chapter_title": "Algebraic Identities",
+        "table_id": "",
+        "section": "Mathematics"
+      }
     ],
     "Geometry": [
-      { "chapter_title": "Elements of Geometry", "table_id": "", "section": "Mathematics" },
-      { "chapter_title": "Lines and Angles", "table_id": "", "section": "Mathematics" },
-      { "chapter_title": "Triangles", "table_id": "", "section": "Mathematics" },
-      { "chapter_title": "Quadrilaterals", "table_id": "", "section": "Mathematics" },
-      { "chapter_title": "Area", "table_id": "", "section": "Mathematics" },
-      { "chapter_title": "Circles", "table_id": "", "section": "Mathematics" },
-      { "chapter_title": "Geometrical Constructions", "table_id": "", "section": "Mathematics" }
+      {
+        "chapter_title": "Elements of Geometry",
+        "table_id": "",
+        "section": "Mathematics"
+      },
+      {
+        "chapter_title": "Lines and Angles",
+        "table_id": "",
+        "section": "Mathematics"
+      },
+      {
+        "chapter_title": "Triangles",
+        "table_id": "",
+        "section": "Mathematics"
+      },
+      {
+        "chapter_title": "Quadrilaterals",
+        "table_id": "",
+        "section": "Mathematics"
+      },
+      {
+        "chapter_title": "Area",
+        "table_id": "",
+        "section": "Mathematics"
+      },
+      {
+        "chapter_title": "Circles",
+        "table_id": "",
+        "section": "Mathematics"
+      },
+      {
+        "chapter_title": "Geometrical Constructions",
+        "table_id": "",
+        "section": "Mathematics"
+      }
     ],
     "Mensuration, Statistics & Probability": [
-      { "chapter_title": "Coordinate Geometry", "table_id": "", "section": "Mathematics" },
-      { "chapter_title": "Mensuration", "table_id": "", "section": "Mathematics" },
-      { "chapter_title": "Statistics", "table_id": "", "section": "Mathematics" },
-      { "chapter_title": "Probability", "table_id": "", "section": "Mathematics" },
-      { "chapter_title": "Proofs in Mathematics", "table_id": "", "section": "Mathematics" }
+      {
+        "chapter_title": "Coordinate Geometry",
+        "table_id": "",
+        "section": "Mathematics"
+      },
+      {
+        "chapter_title": "Mensuration",
+        "table_id": "",
+        "section": "Mathematics"
+      },
+      {
+        "chapter_title": "Statistics",
+        "table_id": "",
+        "section": "Mathematics"
+      },
+      {
+        "chapter_title": "Probability",
+        "table_id": "",
+        "section": "Mathematics"
+      },
+      {
+        "chapter_title": "Proofs in Mathematics",
+        "table_id": "",
+        "section": "Mathematics"
+      }
     ]
   }
 };
-
 export default curriculum;
